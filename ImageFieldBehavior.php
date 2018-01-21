e9880ba7ad583923ef33827c0e493c93226a43dd
Oleg <noneedinmagic@gmail.com>
2018-01-21 17:18:16 +0200

    Slight refactoring of the old code

--- ImageFieldBehavior.php
+++ ImageFieldBehavior.php
@@ -10,11 +10,17 @@ use yii\helpers\Url;
 
 class ImageFieldBehavior extends AttributeBehavior
 {
+	public $imagineClass = '\Imagine\Gd\Imagine';
+
 	public $fileAttribute = 'file';
 	public $filenameAttribute = 'filename';
 	public $filenameLength = 16;
 
-	public $imagineClass = '\Imagine\Gd\Imagine';
+	public $multiple = false;
+	public $multipleSeparator = '|';
+
+	public $optional = false;
+
 	public $imageStorePath;
 	public $imageCachePath;
 	public $imageDefaultUrl;
@@ -28,127 +34,184 @@ class ImageFieldBehavior extends AttributeBehavior
 		];
 	}
 
+	// public function beforeValidate($event){
+	//     if(empty($this->owner->{$this->fileAttribute})){
+	//         if (Yii::$app->request->isPost) {
+	//             $this->owner->{$this->fileAttribute} = UploadedFile::getInstance($this->owner, $this->fileAttribute);
+	//             if($this->owner->{$this->fileAttribute}){
+	//                 if(!empty($this->owner->{$this->filenameAttribute})){
+	//                     $this->afterDelete(NULL);
+	//                 }
+	//                 $this->owner->{$this->filenameAttribute} = Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->owner->{$this->fileAttribute}->extension;
+	//                 return TRUE;
+	//             }
+	//         }
+	//     } elseif($this->owner->{$this->fileAttribute} instanceof UploadedFile){
+	//         if(!empty($this->owner->{$this->filenameAttribute})){
+	//             $this->afterDelete(NULL);
+	//         }
+	//         $this->owner->{$this->filenameAttribute} = Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->owner->{$this->fileAttribute}->extension;
+	//         return TRUE;
+	//     }
+	//     return FALSE;
+	// }
+
+	protected function _getFile(){
+		return $this->owner->{$this->fileAttribute};
+	}
+	protected function _setFile($value){
+		$this->owner->{$this->fileAttribute} = $value;
+	}
+	protected function _getFilename(){
+		return $this->owner->{$this->filenameAttribute};
+	}
+	protected function _setFilename($value){
+		$this->owner->{$this->filenameAttribute} = $value;
+	}
+
+	// public function beforeValidate($event){
+	// 	if(empty($this->owner->{$this->fileAttribute}) && Yii::$app->request->isPost){
+	// 		$this->owner->{$this->fileAttribute} = UploadedFile::getInstance($this->owner, $this->fileAttribute);
+	// 	}
+
+	// 	if($this->owner->{$this->fileAttribute} instanceof UploadedFile){
+	// 		if(!empty($this->owner->{$this->filenameAttribute})){
+	// 			$this->afterDelete(NULL);
+	// 		}
+	// 		$this->owner->{$this->filenameAttribute} = Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->owner->{$this->fileAttribute}->extension;
+	// 		return TRUE;
+	// 	}
+	// 	return FALSE;
+	// }
 	public function beforeValidate($event){
-		if(empty($this->owner->{$this->fileAttribute})){
-            if (Yii::$app->request->isPost) {
-                $this->owner->{$this->fileAttribute} = UploadedFile::getInstance($this->owner, $this->fileAttribute);
-                if($this->owner->{$this->fileAttribute}){
-                	if(!empty($this->owner->{$this->filenameAttribute})){
-                		$this->afterDelete(NULL);
-                	}
-                	$this->owner->{$this->filenameAttribute} = Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->owner->{$this->fileAttribute}->extension;
-                	return TRUE;
-                }
-            }
-		} elseif($this->owner->{$this->fileAttribute} instanceof UploadedFile){
-            if(!empty($this->owner->{$this->filenameAttribute})){
-                $this->afterDelete(NULL);
-            }
-            $this->owner->{$this->filenameAttribute} = Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->owner->{$this->fileAttribute}->extension;
-            return TRUE;
-        }
+		if(empty($this->_getFile()) && Yii::$app->request->isPost){
+			$this->_setFile(UploadedFile::getInstance($this->owner, $this->fileAttribute));
+		}
+
+		if($this->_getFile() instanceof UploadedFile){
+			if(!empty($this->_getFilename())){
+				$this->afterDelete(NULL);
+			}
+			$this->setFilename(Yii::$app->security->generateRandomString($this->filenameLength) . '.' . $this->_getFile()->extension);
+			return TRUE;
+		}
 		return FALSE;
 	}
 
+	// public function beforeSave($event){
+	// 	if (!empty($this->owner->{$this->fileAttribute}) && BaseFileHelper::createDirectory(Yii::getAlias($this->imageStorePath))) {
+	// 		return $this->owner->{$this->fileAttribute}->saveAs($this->imageOriginalPath());
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }
 	public function beforeSave($event){
-        if (!empty($this->owner->{$this->fileAttribute}) && BaseFileHelper::createDirectory(Yii::getAlias($this->imageStorePath))) {
-            return $this->owner->{$this->fileAttribute}->saveAs($this->imageOriginalPath());
-        } else {
-            return false;
-        }
+		if (!empty($this->_getFile()) && BaseFileHelper::createDirectory(Yii::getAlias($this->imageStorePath))) {
+			return $this->_getFile()->saveAs($this->imageOriginalPath());
+		} else {
+			return false;
+		}
 	}
 
 	public function afterDelete($event){
-        $this->dropCache();
-        if(is_file($this->imageOriginalPath())){
-	        unlink($this->imageOriginalPath());
-	    }
+		$this->dropCache();
+		if(is_file($this->imageOriginalPath())){
+			unlink($this->imageOriginalPath());
+		}
 	}
 
-    public function imageUrl($options = [])
-    {
-        return Url::to(str_replace(
-        	Yii::getAlias('@webroot'),
-        	Yii::getAlias('@web'),
-        	Yii::getAlias($this->imagePath($options))));
-    }
+	public function imageUrl($options = [])
+	{
+		return Url::to(str_replace(
+			Yii::getAlias('@webroot'),
+			Yii::getAlias('@web'),
+			Yii::getAlias($this->imagePath($options))));
+	}
 
-    public function imageOriginalPath()
-    {
-        return Yii::getAlias($this->imageStorePath . '/' . $this->owner->{$this->filenameAttribute});
-    }
+	// public function imageOriginalPath()
+	// {
+	// 	return Yii::getAlias($this->imageStorePath . '/' . $this->owner->{$this->filenameAttribute});
+	// }
+	public function imageOriginalPath()
+	{
+		return Yii::getAlias($this->imageStorePath . '/' . $this->_getFilename());
+	}
 
-    protected function imageCachePath()
-    {
-        return Yii::getAlias($this->imageCachePath .'/'. substr($this->owner->{$this->filenameAttribute}, 0, 1) .'/'. substr($this->owner->{$this->filenameAttribute}, 0, 2) .'/'. $this->owner->{$this->filenameAttribute} .'/');
-    }
+	// protected function imageCachePath()
+	// {
+	// 	return Yii::getAlias($this->imageCachePath .'/'. substr($this->owner->{$this->filenameAttribute}, 0, 1) .'/'. substr($this->owner->{$this->filenameAttribute}, 0, 2) .'/'. $this->owner->{$this->filenameAttribute} .'/');
+	// }
+	protected function imageCachePath()
+	{
+		return Yii::getAlias($this->imageCachePath .'/'. substr($this->_getFilename(), 0, 1) .'/'. substr($this->_getFilename(), 0, 2) .'/'. $this->_getFilename() .'/');
+	}
 
-    public function dropCache()
-    {
-        BaseFileHelper::removeDirectory($this->imageCachePath());
-        $levels = BaseFileHelper::normalizePath($this->imageCachePath() . '../..');
-        if(is_dir($levels = BaseFileHelper::normalizePath($this->imageCachePath() . '../..')) && !BaseFileHelper::findFiles($levels)){
-        	BaseFileHelper::removeDirectory($levels);
-        } elseif(is_dir($levels = BaseFileHelper::normalizePath($this->imageCachePath() . '..')) && !BaseFileHelper::findFiles($levels)){
-        	BaseFileHelper::removeDirectory($levels);
-        }
-    }
+	public function dropCache()
+	{
+		BaseFileHelper::removeDirectory($this->imageCachePath());
+		$levels = BaseFileHelper::normalizePath($this->imageCachePath() . '../..');
+		if(is_dir($levels = BaseFileHelper::normalizePath($this->imageCachePath() . '../..')) && !BaseFileHelper::findFiles($levels)){
+			BaseFileHelper::removeDirectory($levels);
+		} elseif(is_dir($levels = BaseFileHelper::normalizePath($this->imageCachePath() . '..')) && !BaseFileHelper::findFiles($levels)){
+			BaseFileHelper::removeDirectory($levels);
+		}
+	}
 
-    //TODO: remove $normalize
-    //TODO: edit $fullresult
-    public function imagePath($options = [])
-    {
-        if(!is_file($this->imageOriginalPath())){
-        	return NULL;
-        }
-        if(!empty($options['original'])){
-            return $this->imageOriginalPath();
-        }
+	//TODO: remove $normalize
+	//TODO: edit $fullresult
+	public function imagePath($options = [])
+	{
+		if(!is_file($this->imageOriginalPath())){
+			return NULL;
+		}
+		if(!empty($options['original'])){
+			return $this->imageOriginalPath();
+		}
 
-        $options = array_merge([
-            'width' => null,
-            'height' => null,
-            'preserveAspectRatio' => true,
-            'inset' => false,
-            'format' => 'jpg',
-            'jpeg_quality' => 80,
-            'png_compression_level' => 7,
-            'rewrite' => false,
-            'path' => $this->imageCachePath(),
-            'filename' => '{width}x{height}.{ext}',
-        ], $options);
+		$options = array_merge([
+			'width' => null,
+			'height' => null,
+			'preserveAspectRatio' => true,
+			'inset' => false,
+			'format' => 'jpg',
+			'jpeg_quality' => 80,
+			'png_compression_level' => 7,
+			'rewrite' => false,
+			'path' => $this->imageCachePath(),
+			'filename' => '{width}x{height}.{ext}',
+		], $options);
 
-        $filename = str_replace(['{width}', '{height}', '{ext}'], [
-            (empty($options['width']) ? '' : $options['width']),        //width
-            (empty($options['height']) ? '' : $options['height']),      //height
-            (empty($options['format']) ? '' : $options['format']),      //format
-            // ()
-        ], $options['filename']);
+		$filename = str_replace(['{width}', '{height}', '{ext}'], [
+			(empty($options['width']) ? '' : $options['width']),        //width
+			(empty($options['height']) ? '' : $options['height']),      //height
+			(empty($options['format']) ? '' : $options['format']),      //format
+			// ()
+		], $options['filename']);
 
-        $fullpath = BaseFileHelper::normalizePath($options['path']);
-        $fullname = BaseFileHelper::normalizePath($fullpath .'/'. $filename);
+		$fullpath = BaseFileHelper::normalizePath($options['path']);
+		$fullname = BaseFileHelper::normalizePath($fullpath .'/'. $filename);
 
-        if($options['rewrite'] || !file_exists($fullname)){
-            $image = (new $this->imagineClass)->open($this->imageOriginalPath());
+		if($options['rewrite'] || !file_exists($fullname)){
+			$image = (new $this->imagineClass)->open($this->imageOriginalPath());
 
-            $box = $image->getSize();
-            if(empty($options['width'])){
-                if($options['height']){
-                    $box = $box->scale($options['height'] / $box->getHeight());
-                }
-            } elseif(empty($options['height'])){
-                $box = $box->scale($options['width'] / $box->getWidth());
-            } else {
-                $box = new \Imagine\Image\Box($options['width'], $options['height']);
-            }
+			$box = $image->getSize();
+			if(empty($options['width'])){
+				if($options['height']){
+					$box = $box->scale($options['height'] / $box->getHeight());
+				}
+			} elseif(empty($options['height'])){
+				$box = $box->scale($options['width'] / $box->getWidth());
+			} else {
+				$box = new \Imagine\Image\Box($options['width'], $options['height']);
+			}
 
-            $image = $image->thumbnail($box);
+			$image = $image->thumbnail($box);
 
-            BaseFileHelper::createDirectory($fullpath);
+			BaseFileHelper::createDirectory($fullpath);
 
-            $image->save($fullname);
-        }
+			$image->save($fullname);
+		}
 
-        return BaseFileHelper::normalizePath($fullname, '/');;
-    }
+		return BaseFileHelper::normalizePath($fullname, '/');;
+	}
 }
