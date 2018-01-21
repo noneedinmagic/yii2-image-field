b268f83827390596de16ff577c67eab0f1b82658
Oleg <noneedinmagic@gmail.com>
2018-01-21 17:36:41 +0200

    Tweak optional variant accordingly to the main one

--- ImageFieldOptionalBehavior.php
+++ ImageFieldOptionalBehavior.php
@@ -11,38 +11,45 @@ class ImageFieldOptionalBehavior extends ImageFieldBehavior
 	}
 
 	public function beforeSave($event){
-		return empty($this->owner->{$this->fileAttribute}) || parent::beforeSave($event);
+        // return empty($this->owner->{$this->fileAttribute}) || parent::beforeSave($event);
+		return empty($this->_getFile()) || parent::beforeSave($event);
 	}
 
 	public function afterDelete($event){
-		if(!empty($this->owner->{$this->filenameAttribute})){
+        // if(!empty($this->owner->{$this->filenameAttribute})){
+		if(!empty($this->_getFilename())){
 			parent::afterDelete($event);
 		}
 	}
 
     public function imageUrl($options = [])
     {
-        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageUrl($options);
+        // return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageUrl($options);
+        return empty($this->_getFilename()) ? NULL : parent::imageUrl($options);
     }
 
     public function imageOriginalPath()
     {
-        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageOriginalPath();
+        // return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageOriginalPath();
+        return empty($this->_getFilename()) ? NULL : parent::imageOriginalPath();
     }
 
     protected function imageCachePath()
     {
-        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageCachePath();
+        // return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageCachePath();
+        return empty($this->_getFilename()) ? NULL : parent::imageCachePath();
     }
 
     public function dropCache()
     {
-		if(!empty($this->owner->{$this->filenameAttribute})){
+        // if(!empty($this->owner->{$this->filenameAttribute})){
+		if(!empty($this->_getFilename())){
 			parent::dropCache();
 		}
     }
 
     public function imagePath($options = []){
-        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imagePath($options);
+        // return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imagePath($options);
+        return empty($this->_getFilename()) ? NULL : parent::imagePath($options);
     }
 }
